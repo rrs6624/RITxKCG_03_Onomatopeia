22c2a641d8ecd328300c1393f609947966e737d8
KawakamiRiku <0818riku0818riku@gmail.com>
2026-05-31 11:13:49 +0900

    resultSceneの作成

---
+++ パチンパチン/Assets/Scenes/Riku/result/ScoretextManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;  
+
+public class ScoretextManager : MonoBehaviour
+{
+
+
+    public GameObject score_object = null; // Textオブジェクト
+
+    // 初期化
+    void Start()
+    {
+    }
+
+    // 更新
+    void Update()
+    {
+        // オブジェクトからTextコンポーネントを取得
+        Text score_text = score_object.GetComponent<Text>();
+        // テキストの表示を入れ替える
+        score_text.text = "000000";
+    }
+}
